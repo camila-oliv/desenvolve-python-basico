4c6f0976993dd290b6467cfbeecfe00e3ae522ca
camila-oliv <camila.oliveira@pditabira.com>
2024-04-20 20:29:01 -0300

    Continuação

---
+++ modulo 2/aula4_questao2.py
@@ -0,0 +1,8 @@
+# Dados Entrada
+fahrenheit = int(input("Digite a temperatura em Fahrenheit: "))
+
+#Processamento
+celsius = (fahrenheit - 32) * 5 / 9
+
+# Dados de Saída
+print(f"{fahrenheit} graus Fahrenheit são {int(celsius)} graus Celsius.")
