094ec6a00425e5e5bb878a405671e83688e4d3bc
camila-oliv <camila.oliveira@pditabira.com>
2024-04-27 15:13:45 -0300

    Atualização

--- modulo 3/aula4_questao4.py
+++ modulo 3/aula4_questao4.py
@@ -6,12 +6,17 @@ peso_pacote = int(input('Digite o peso em kg: '))
 #Distância até 100 km: R$1 por kg.
 #Distância entre 101 e 300 km: R$1.50 por kg.
 #Distância acima de 300 km: R$2 por kg.
+#Acrescente uma taxa de R$10 para pacotes com peso superior a 10 kg
 
 a = (distancia <= 100) and (peso_pacote * 1)
 b = (distancia >= 101) and (distancia<=300) and (peso_pacote * 1.50)
 c = (distancia <= 300)and (peso_pacote * 2)
 
 frete = a or b or c 
+
+if peso_pacote >10:
+    frete += 10
+
 
 #Dados de Saída
 print(f'Seu frete é {frete}')
