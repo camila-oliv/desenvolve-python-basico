e8d9f4d2b1ffde92224783c61e62392c4c1af5d0
camila-oliv <camila.oliveira@pditabira.com>
2024-05-28 15:52:07 -0300

    Conclusão

--- modulo 2/aula2_questao2.py
+++ modulo 2/aula2_questao2.py
@@ -1,13 +1,21 @@
+#Transforme o seguinte algoritmo em código. Lembre-se das funções print e 
+#type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra
+#variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra 
+#variável. Imprima todas as variáveis na ordem de criação e imprima 
+#também seus tipos.
+
+#Dados de Entrada
 x="O resultado é"
 y=10
 w=3.5
-print(y+w)
-c=13.5
-print(x)
-print(type(x))
-print(y)
-print(type(y))
-print(w)
-print(type(w))
-print(c)
-print(type(c))
+
+#Processamento
+soma = (y+w)
+
+#Dados de Saída
+print(x,type(x))
+print(y,type(y))
+print(w,type(w))
+print(soma,type(soma))
