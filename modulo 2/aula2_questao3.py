178cad02fee256d6562e4c45cd16686f73382c65
camila-oliv <camila.oliveira@pditabira.com>
2024-05-28 15:55:10 -0300

    conClusão

--- modulo 2/aula2_questao3.py
+++ modulo 2/aula2_questao3.py
@@ -1,9 +1,17 @@
+#Dadas duas variáveis v1 = 10 e v2 = 20, utilize uma terceira variável 
+#para trocar os valores entre as duas variáveis. Ou seja, ao final v1 
+#terá o valor de v2, e v2 o valor de v1. Você deve usar uma variável 
+#auxiliar de troca, não podendo atribuir os novos valores diretamente.
+
+#Dados de Entrada
 v1=10
 v2=20
 
+#Processamento
 x = v1
 v1 = v2
 v2 = x
 
+#Dados de Saída
 print(f"Valor V1: {v1}")
-print(f"Valor V1: {v2}")
+print(f"Valor V2: {v2}")
